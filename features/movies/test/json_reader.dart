e2311d828ded699cdcbb78dc44926815696cf6df
Rifqi <rifqi.fadhlillah@gmail.com>
2023-10-27 19:51:36 +0700

    fix: update path readJson in module movies

--- features/movies/test/json_reader.dart
+++ features/movies/test/json_reader.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 String readJson(String name) {
   var dir = Directory.current.path;
   if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
+    dir = dir.replaceAll('/features/movies/test', '');
   }
   return File('$dir/test/$name').readAsStringSync();
 }
