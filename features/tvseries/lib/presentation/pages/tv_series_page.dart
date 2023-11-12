bd0f736fd0b463fa2b2d90b2b1db752bd060e803
Rifqi <rifqi.fadhlillah@gmail.com>
2023-11-12 16:39:29 +0700

    feat: add Firebase Crashlytics

--- features/tvseries/lib/presentation/pages/tv_series_page.dart
+++ features/tvseries/lib/presentation/pages/tv_series_page.dart
@@ -2,7 +2,6 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:tvseries/presentation/bloc/tv_series_list/tv_series_list_bloc.dart';
 import 'package:tvseries/tvseries.dart';
 
 class TVSeriesPage extends StatefulWidget {
